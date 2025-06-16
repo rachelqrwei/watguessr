39661624a73f70f3cc21e140624c79c35e8c0895
reiwei <rachelqingranwei@gmail.com>
2025-06-15 17:09:28 -0700

    initial yml files added

--- src/main/java/com/gooners/watguessr/WatGuessr.java
+++ src/main/java/com/gooners/watguessr/WatGuessr.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class WatGuessr {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WatGuessr.class, args);
 	}
-
 }
+
