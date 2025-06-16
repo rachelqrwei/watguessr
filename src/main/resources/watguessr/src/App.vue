2f2bb413798aff98b609b4333a0005b51531ffb4
Leanne <sooyeunleanne@gmail.com>
2025-06-15 17:34:49 -0700

    structure of home page done

--- src/main/resources/watguessr/src/App.vue
+++ src/main/resources/watguessr/src/App.vue
@@ -7,41 +7,58 @@ const route = useRoute()
 
 <template>
   <header>
-    <div>
-      <div class="flex-container">
-        <img src="../../src/assets/image/location_on.png" alt="Logo"/>
-        <RouterLink to="/" class="website-title">WATGUESSR.IO</RouterLink>
-      </div>
+    <div class="website-title-container flex-container">
+      <img src="../../src/assets/images/App/location_on.png" alt="Logo" :style="{height: '50px'}"/>
+      <RouterLink to="/" class="website-title">WATGUESSR.IO</RouterLink>
+    </div>
 
-      <h4>MAIN</h4>
+    <h4>MAIN</h4>
 
-      <div :class="['nav-option flex-container', route.path === '/play' && 'selected']">
-        <img src="../../src/assets/image/play-icon.png" alt="Play"/>
-        <RouterLink to="/play">PLAY WATGUESSR</RouterLink>
-      </div>
+    <div :class="['nav-option flex-container', route.path === '/play' && 'selected']">
+      <img src="../../src/assets/images/App/play-icon.png" alt="Play"/>
+      <RouterLink to="/play" id="router-link">PLAY WATGUESSR</RouterLink>
+    </div>
 
-      <div :class="['nav-option flex-container', route.path === '/leaderboard' && 'selected']">
-        <img src="../../src/assets/image/leaderboard-icon.png" alt="Leaderboard"/>
-        <RouterLink to="/leaderboard">LEADERBOARD</RouterLink>
-      </div>
+    <div :class="['nav-option flex-container', route.path === '/leaderboard' && 'selected']">
+      <img src="../../src/assets/images/App/leaderboard-icon.png" alt="Leaderboard"/>
+      <RouterLink to="/leaderboard" id="router-link">LEADERBOARD</RouterLink>
+    </div>
 
-      <div :class="['nav-option flex-container', route.path === '/profile' && 'selected']">
-        <img src="../../src/assets/image/profile-icon.png" alt="Profile"/>
-        <RouterLink to="/profile">PROFILE</RouterLink>
-      </div>
-    
-      <div :class="['nav-option flex-container', route.path === '/settings' && 'selected']">
-        <img src="../../src/assets/image/settings-icon.png" alt="Settings"/>
-        <RouterLink to="/settings">SETTINGS</RouterLink>
-      </div>
+    <div :class="['nav-option flex-container', route.path === '/profile' && 'selected']">
+      <img src="../../src/assets/images/App/profile-icon.png" alt="Profile"/>
+      <RouterLink to="/profile" id="router-link">PROFILE</RouterLink>
+    </div>
+  
+    <div :class="['nav-option flex-container', route.path === '/settings' && 'selected']">
+      <img src="../../src/assets/images/App/settings-icon.png" alt="Settings"/>
+      <RouterLink to="/settings" id="router-link">SETTINGS</RouterLink>
     </div>
   </header>
 
-  <RouterView />
+  <div class="main-content">
+    <RouterView />
+  </div>
+  
 
 </template>
 
-<style>
+<style scoped>
+header {
+  position: fixed;
+  width: 30vw;
+  height: 100vh;
+  box-shadow: 1px 4px 20px rgba(0, 0, 0, 0.25);
+}
+
+.website-title-container {
+  align-items: center;
+}
+
+.website-title {
+  font-size: 32px;
+  font-weight: bold;
+}
+
 h4 {
   font-weight: bold;
   color: var(--color-zinc-400);
@@ -51,24 +68,26 @@ h4 {
 
 .nav-option {
   margin: 10px;
-  height: 36px;
+  height: 30px;
   align-items: center;
   gap: 26px;
   padding: 30px;
-  border-radius: 15px;
+  border-radius: 5px;
 }
 
 .nav-option > img {
-  height: 36px;
-}
-
-.nav-option > RouterLink {
-  font-size: 16px;
-  font-weight: bold;
+  height: 30px;
 }
 
 .nav-option:hover, .nav-option.selected {
   box-shadow: inset 0 0 0 2px white;
   background-color: rgba(139, 164, 177, 0.27);
+}
+
+.main-content {
+  position: absolute;
+  width: 70vw;
+  left: 30vw;
+  padding: 50px;
 }
 </style>
--- src/main/resources/watguessr/src/views/Home.vue
+++ src/main/resources/watguessr/src/views/Home.vue
@@ -1,14 +1,154 @@
+<script setup>
+import Header from '../components/Header.vue';
+
+</script>
 <template>
-<div>
-</div>
+    <Header />
+    <div class="flex-container home-description-container">
+        <div class="placeholder-square"></div>
+
+        <div :style="{width: '50%'}">
+            <div class="title">
+                <span class="highlight">WATGUESSR</span><span>.IO</span>
+            </div>
+            <p class="subtitle">
+                The hit game Geoguessr, built specifically for the University of Waterloo.
+            </p>
+            <hr />
+        </div>
+    </div>
+
+    <div :style="{marginTop: '50px'}">
+        <h4>PLAY WATGUESSR</h4>
+        <div class="play-options-container flex-container">
+            <div class="player-1-trapezoid">
+                <div class="play-option-container">
+                    <h3>SOLO</h3>
+                    <p>PLAY BY YOURSELF UNTIL YOUR POINTS RUN OUT!</p>
+                </div>
+            </div>
+
+            <div class="player-2-trapezoid">
+                <div class="play-option-container">
+                    <h3>DUELS</h3>
+                    <p>PLAY AGAINST A FRIEND AND COMPETE FOR POINTS!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 
 </script>
-<style>
-.website-title {
+<style scoped>
+.home-description-container {
+    margin-top: 100px;
+    align-items: center;
+}
+
+span {
+    font-size: 50px;
     font-weight: bold;
-    font-size: 32px;
+    color: var(--light-grey)
+}
+
+.highlight {
+    color: var(--yellow);
+}
+
+.subtitle {
+    font-size: 20px;
+    color: var(--light-grey);
+    line-height: 30px;
+    font-weight: bold;
+}
+
+hr {
+    margin-top: 20px;
+    color: var(--light-grey);
+    border-width: 2px;
+    width: 25%;
+}
+
+.placeholder-square {
+    width: 235px;
+    height: 235px;
+    background-color: var(--light-grey); /* Or any color */
+    border: 2px dashed #ccc; /* Optional: dashed border */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #666;
+    margin: 30px;
+}
+
+.player-1-trapezoid {
+    position: relative;
+    width: 350px;
+    height: 150px;
+    clip-path: polygon(0 0, 100% 0, 80% 100%, 0% 100%);
+    border-radius: 10px 5px 20px 10px;
+    padding: 20px;
+    overflow: hidden; /* important to clip the ::before content */
+    z-index: 0;
+}
+
+.player-1-trapezoid::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: var(--player-1-gradient);
+    opacity: 0.8;
+    z-index: -1;
+}
+
+.player-2-trapezoid {
+    position: relative;
+    width: 350px;
+    height: 150px;
+    left: -50px;
+    clip-path: polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%);
+    border-radius: 10px 5px 20px 10px;
+    padding: 20px;
+    padding-left: 80px;
+    overflow: hidden; /* important to clip the ::before content */
+    z-index: 0;
+}
+
+.player-2-trapezoid::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: var(--player-2-gradient);
+    opacity: 0.8;
+    z-index: -1;
+}
+
+.play-option-container {
+    width: 80%;
+    font-size: 14px;
+}
+
+.play-option-container h3{
+    font-weight: bold;
+    font-size: 35px;
+}
+
+.play-option-container p {
+    font-size: 14px;
+    font-weight: bold;
+}
+
+h4 {
+    font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 10px;
 }
 
 </style>
