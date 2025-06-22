991c91d5722ac694263f62847a7f95353ae571d8
Stanley Wang <s927wang@uwaterloo.ca>
2025-06-22 16:58:45 -0700

    changed schema to use watguessr instead of public and enforced manual database creation for initial setup

--- src/main/resources/db/utility-scripts/create-db-postgresql.sql
+++ src/main/resources/db/utility-scripts/create-db-postgresql.sql
@@ -1,27 +1,37 @@
+-- Create Database Manually first.Creating databases in Postgres is a 
+--      little bit “manual” there’s no fully pure SQL transactional way to 
+--      do “create-if-not-exists” for databases.
+-- 
+-- CREATE DATABASE watguessr;
 
-SELECT 'CREATE DATABASE watguessr'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'watguessr')\gexec
-
+-- Create user if not exists
+-- Can run on any database. The pg_catalog schema is shared across all databases in a PostgreSQL instance.
 DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT FROM pg_catalog.pg_roles 
-    WHERE rolname = 'watuser'
-  ) THEN
-    CREATE USER watuser WITH PASSWORD 'goon';
-  END IF;
-END
+    BEGIN
+        IF NOT EXISTS (
+            SELECT FROM pg_catalog.pg_roles
+            WHERE rolname = 'watuser'
+        ) THEN
+            CREATE USER watuser WITH PASSWORD 'goon';
+        END IF;
+    END
 $$;
+
+-- Connect to watguessr database (in psql client)
+-- \connect watguessr
+
+CREATE SCHEMA IF NOT EXISTS watguessr;
 
 GRANT ALL PRIVILEGES ON DATABASE watguessr TO watuser;
 
-\c watguessr
+-- Connect to watguessr database again (in psql client)
+-- \connect watguessr
 
-GRANT ALL ON SCHEMA public TO watuser;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO watuser;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO watuser;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO watuser;
+GRANT ALL ON SCHEMA watguessr TO watuser;
+GRANT ALL ON ALL TABLES IN SCHEMA watguessr TO watuser;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA watguessr TO watuser;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA watguessr TO watuser;
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO watuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO watuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO watuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA watguessr GRANT ALL ON TABLES TO watuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA watguessr GRANT ALL ON SEQUENCES TO watuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA watguessr GRANT ALL ON FUNCTIONS TO watuser;
