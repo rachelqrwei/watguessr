7f2bce501f6f955e89c45788c3d391781e272961
kennywu <wukenny0126@gmail.com>
2025-06-25 20:01:31 -0700

    kenny: Set up for Docker

--- src/main/resources/db/utility-scripts/create-db-postgresql.sql
+++ src/main/resources/db/utility-scripts/create-db-postgresql.sql
@@ -2,7 +2,7 @@
 --      little bit “manual” there’s no fully pure SQL transactional way to 
 --      do “create-if-not-exists” for databases.
 -- 
--- CREATE DATABASE watguessr;
+CREATE DATABASE watguessr;
 
 -- Create user if not exists
 -- Can run on any database. The pg_catalog schema is shared across all databases in a PostgreSQL instance.
@@ -17,12 +17,12 @@ DO $$
     END
 $$;
 
+GRANT ALL PRIVILEGES ON DATABASE watguessr TO watuser;
+
 -- Connect to watguessr database (in psql client)
 -- \connect watguessr
 
 CREATE SCHEMA IF NOT EXISTS watguessr;
-
-GRANT ALL PRIVILEGES ON DATABASE watguessr TO watuser;
 
 -- Connect to watguessr database again (in psql client)
 -- \connect watguessr
