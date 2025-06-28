4543f1f3e724f4cdfdcbcab729350c267db7d1b6
reiwei <rachelqingranwei@gmail.com>
2025-06-28 16:32:03 -0700

    reiwei: fixed guess controller and added calculatepoints formula

--- src/main/java/com/gooners/watguessr/service/GameRoundService.java
+++ src/main/java/com/gooners/watguessr/service/GameRoundService.java
@@ -1,6 +1,8 @@
 package com.gooners.watguessr.service;
 
+import com.gooners.watguessr.entity.Game;
 import com.gooners.watguessr.entity.GameRound;
+import com.gooners.watguessr.entity.Round;
 import com.gooners.watguessr.repository.GameRoundRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Service
 @Transactional
 public class GameRoundService {
+
     private final GameRoundRepository gameRoundRepository;
 
     public GameRoundService(GameRoundRepository gameRoundRepository) {
@@ -18,7 +21,7 @@ public class GameRoundService {
     }
 
     public void create(GameRound gameRound) {
-        this.gameRoundRepository.create(gameRound);
+        gameRoundRepository.create(gameRound);
     }
 
     public void update(GameRound gameRound) {
@@ -27,6 +30,11 @@ public class GameRoundService {
 
     public void delete(UUID id) {
         this.gameRoundRepository.delete(id);
+    }
+
+    public Game getGameFromRound(Round round) {
+        GameRound gameRound = gameRoundRepository.findByRound(round);
+        return gameRound.getGameId();
     }
 
     public GameRound findById(UUID id) {
