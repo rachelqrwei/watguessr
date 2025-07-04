fe4c72d5227b6fbbe3427529dbc40756a31a0b09
Leanne <sooyeunleanne@gmail.com>
2025-07-03 21:47:14 -0700

    Play.ScoreTracker.vue

--- src/main/resources/watguessr/src/views/Play.vue
+++ src/main/resources/watguessr/src/views/Play.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue';
 import PlayStopwatch from "@/views/play-components/Play.Stopwatch.vue";
 import PlayMapView from '@/views/play-components/Play.Map.vue';
 import PlayImageView from '@/views/play-components/Play.Image.vue';
+import PlayScoreTracker from "@/views/play-components/Play.ScoreTracker.vue";
 
 const currentView = ref("Map");
 
@@ -24,19 +25,7 @@ function changeView(nextView) {
     <PlayImageView />
   </div>
 
-  <div class="player-score-tracker-container">
-    <div class="player-score-tracker-1">
-      <h1 class="player-name">Name</h1>
-      <p>2000 pts</p>
-      <div class="player-score-progress-bar" />
-    </div>
-
-    <div class="player-score-tracker-2">
-      <h1 class="player-name">Name</h1>
-      <p>2000 pts</p>
-      <div class="player-score-progress-bar" />
-    </div>
-  </div>
+  <PlayScoreTracker />
 </template>
 <style scoped>
 .view-change-button {
@@ -53,48 +42,5 @@ function changeView(nextView) {
   font-weight: bold;
   font-size: 18px;
   cursor: pointer;
-}
-
-.player-score-tracker-container {
-  position: fixed;
-  bottom: 0;          /* Aligns to bottom */
-  left: 0;            /* Aligns to left edge */
-  width: 100%;        /* Full width */
-  color: white;
-  padding: 16px;
-  z-index: 9999;
-
-  display: flex;
-  justify-content: space-between; /* Push children to left & right */
-  align-items: center;
-}
-
-.player-name {
-  font-size: 20px;
-  font-weight: bold;
-}
-
-.player-score-tracker-1 {
-}
-
-.player-score-tracker-2 {
-  text-align: right;
-}
-
-.player-score-tracker-1,
-.player-score-tracker-2 {
-  max-width: 45%;
-  background: #333;
-  padding: 20px;
-  min-width: 30%;
-  font-size: 18px;
-  border-radius: 30px;
-}
-
-.player-score-progress-bar {
-  height: 8px;
-  background-color: #aaa;
-  margin-top: 8px;
-  border-radius: 4px;
 }
 </style>
---
+++ src/main/resources/watguessr/src/views/play-components/Play.ScoreTracker.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { onMounted, ref, computed } from "vue";
+
+//TODO: connect player info to backend
+const player1Name = "Name 1";
+const player2Name = "Name 2";
+
+const player1Score = 40;
+const player2Score = 60;
+
+const player1ScorePercentage = computed(() => {
+  return Math.floor((player1Score * 100) / (player1Score + player2Score))
+})
+
+const player2ScorePercentage = computed(() => {
+  return Math.floor((player2Score * 100) / (player1Score + player2Score))
+})
+</script>
+
+<template>
+  <div class="player-score-tracker-container">
+    <div class="player-score-tracker-1">
+      <div class="player-score-text">
+        <h1 class="player-name">{{ player1Name }}</h1>
+        <p>{{ player1Score }} pts</p>
+      </div>
+
+      <div class="player-score-progress-bar"
+           :style="{
+             width: player1ScorePercentage + '%',
+             background: 'var(--player-1-gradient)'
+           }"
+      />
+    </div>
+
+    <div class="player-score-tracker-2">
+      <div class="player-score-text">
+        <h1 class="player-name">{{ player2Name }}</h1>
+        <p>{{ player2Score }} pts</p>
+      </div>
+
+      <div class="player-score-progress-bar"
+           :style="{
+             width: player2ScorePercentage + '%',
+             background: 'var(--player-2-gradient)'
+           }"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.player-score-tracker-container {
+  position: fixed;
+  bottom: 0;          /* Aligns to bottom */
+  left: 0;            /* Aligns to left edge */
+  width: 100%;        /* Full width */
+  color: white;
+  padding: 16px;
+  z-index: 9999;
+
+  display: flex;
+  justify-content: space-between; /* Push children to left & right */
+  align-items: center;
+}
+
+.player-name {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.player-score-tracker-1 {
+}
+
+.player-score-tracker-2 {
+  text-align: right;
+  direction: rtl;
+}
+
+.player-score-tracker-1,
+.player-score-tracker-2 {
+  max-width: 45%;
+  background: #333;
+  padding: 20px;
+  min-width: 30%;
+  font-size: 18px;
+  border-radius: 30px;
+}
+
+.player-score-progress-bar {
+  height: 10px;
+  background-color: #aaa;
+  margin-top: 10px;
+  border-radius: 5px;
+}
+
+.player-score-text {
+  direction: ltr;
+}
+</style>
