1ab4dc879470855f250bf4b566212f1844a7a29a
Leanne <sooyeunleanne@gmail.com>
2025-07-27 15:18:08 -0700

    making vuex modules to work in ts

--- src/main/resources/watguessr/src/main.ts
+++ src/main/resources/watguessr/src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import store from './stores'
+
 
 import App from './App.vue'
 import router from './router'
@@ -9,12 +11,12 @@ import router from './router'
 // FontAwesome imports
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { 
-  faPlay, 
-  faTrophy, 
-  faUser, 
-  faCog, 
-  faMapMarkerAlt, 
+import {
+  faPlay,
+  faTrophy,
+  faUser,
+  faCog,
+  faMapMarkerAlt,
   faChevronDown,
   faStar,
   faClipboard,
@@ -26,6 +28,7 @@ import {
 library.add(faPlay, faTrophy, faUser, faCog, faMapMarkerAlt, faChevronDown, faStar, faClipboard, faUsers, faImage)
 
 const app = createApp(App)
+  .use(store);
 
 app.use(createPinia())
 app.use(router)
---
+++ src/main/resources/watguessr/src/types/index.d.ts
@@ -0,0 +1,4 @@
+// types/vuex.d.ts or src/types/vuex.d.ts
+declare module 'vuex' {
+  export * from 'vuex/types/index.d.ts';
+}
