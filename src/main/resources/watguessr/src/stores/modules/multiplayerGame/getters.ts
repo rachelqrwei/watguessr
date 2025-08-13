cfd73d1b7dc2da2cc096d4a6edd8c37f5dec482c
synlne <sooyeunleanne@gmail.com>
2025-08-13 01:43:08 -0700

    wip getters.ts

--- src/main/resources/watguessr/src/stores/modules/multiplayerGame/getters.ts
+++ src/main/resources/watguessr/src/stores/modules/multiplayerGame/getters.ts
@@ -1,8 +1,9 @@
 // src/stores/modules/game/getters.ts
 import type { GetterTree } from 'vuex';
-import type { GameState } from './state';
+import type { MultiplayerGameState } from './state';
+import type {RootState} from "@/stores";
 
-export const getters: GetterTree<GameState, RootState> = {
+export const getters: GetterTree<MultiplayerGameState, RootState> = {
   getMultiplayerGame_players: (state) => state.multiplayerGame_players,
   getMultiplayerGame_currentRound: (state) => state.multiplayerGame_currentRound,
   getMultiplayerGame_maxRounds: (state) => state.multiplayerGame_currentRound,
