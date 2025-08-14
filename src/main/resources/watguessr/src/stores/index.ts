5f5e7121cb8e9990398f86d5623b2c79c62ab2a6
synlne <sooyeunleanne@gmail.com>
2025-08-13 20:30:51 -0700

    stores/index.ts

--- src/main/resources/watguessr/src/stores/index.ts
+++ src/main/resources/watguessr/src/stores/index.ts
@@ -5,12 +5,17 @@ import guessModule from './modules/guess/index';
 import leaderboardModule from './modules/leaderboard';
 import userModule from './modules/user/index';
 import buildingModule from './modules/building/index';
+import GameInfoModule from './modules/gameInfo/index';
+import singleplayerGameModule from "@/stores/modules/singleplayerGame";
+import MultiplayerGameModule from "@/stores/modules/multiplayerGame";
+
 import type { RoundState } from './modules/round/state';
 import type { singleplayerGameState } from '@/stores/modules/singleplayerGame/state';
 import type { GuessState } from './modules/guess/state';
 import type { UserState } from './modules/user/state';
 import type { BuildingState } from '@/stores/modules/building/state';
-import singleplayerGameModule from "@/stores/modules/singleplayerGame";
+import type { MultiplayerGameState } from "@/stores/modules/multiplayerGame/state.ts";
+import type { GameInfoState } from "@/stores/modules/gameInfo/state.ts";
 
 export interface LeaderboardState {
   leaderboardData: any;
@@ -21,21 +26,25 @@ export interface LeaderboardState {
 
 export interface RootState {
   round: RoundState;
-  singleplayerGame: singleplayerGameState;
   guess: GuessState;
   leaderboard: LeaderboardState;
   user: UserState;
   building: BuildingState;
+  gameInfo: GameInfoState;
+  singleplayerGame: singleplayerGameState;
+  multiplayerGame: MultiplayerGameState;
 }
 
 const store = createStore<RootState>({
   modules: {
-    singleplayerGame: singleplayerGameModule as Module<singleplayerGameState, RootState>,
     round: roundModule as Module<RoundState, RootState>,
     guess: guessModule as Module<GuessState, RootState>,
     leaderboard: leaderboardModule as Module<LeaderboardState, RootState>,
     user: userModule as Module<UserState, RootState>,
     building: buildingModule as Module<BuildingState, RootState>,
+    gameInfo: GameInfoModule as Module<GameInfoState, RootState>,
+    singleplayerGame: singleplayerGameModule as Module<singleplayerGameState, RootState>,
+    multiplayerGame: MultiplayerGameModule as Module<MultiplayerGameState, RootState>
   },
 });
 
