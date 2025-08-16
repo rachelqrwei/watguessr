af1235cae818d17050bf9a9c6db481a8245e2369
kennywu <wukenny0126@gmail.com>
2025-08-16 00:56:29 -0700

    kenny: Implemented JWT Tokenization

--- src/main/java/com/gooners/watguessr/controller/UserController.java
+++ src/main/java/com/gooners/watguessr/controller/UserController.java
@@ -1,50 +1,27 @@
 package com.gooners.watguessr.controller;
 
-import com.gooners.watguessr.dto.UserSignupDto;
-import com.gooners.watguessr.dto.UserLoginDto;
-
-import com.gooners.watguessr.entity.User;
-import com.gooners.watguessr.mapper.UserMapper;
-import com.gooners.watguessr.service.UserService;
-import com.gooners.watguessr.service.EmailVerificationService;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-
-import com.gooners.watguessr.dto.QueryResults;
-import com.gooners.watguessr.dto.LeaderboardUser;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gooners.watguessr.dto.LeaderboardUser;
+import com.gooners.watguessr.dto.QueryResults;
+import com.gooners.watguessr.entity.User;
+import com.gooners.watguessr.service.UserService;
 
 @RestController
 @RequestMapping("api/user")
 public class UserController {
     
     private final UserService userService;
-    private final UserMapper userMapper;
-    private final EmailVerificationService emailVerificationService;
-    public UserController(UserService userService, UserMapper userMapper, EmailVerificationService emailVerificationService) {
+
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
-        this.emailVerificationService = emailVerificationService;
-    }
-
-    @PostMapping(value = "/register")
-    public void register(@RequestBody @Valid User user) {
-        this.userService.create(user);
-    }
-
-    @PutMapping("/login")
-    public ResponseEntity<?> login(@RequestBody @Valid UserLoginDto loginDto) {
-        User user = userService.login(loginDto.getUsername(), loginDto.getPassword());
-        return ResponseEntity.ok(userMapper.toDto(user));
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid UserSignupDto dto) {
-        userService.signup(dto);
-        return ResponseEntity.ok("Account created");
     }
 
     @GetMapping(value = "/{id}")
@@ -57,17 +34,6 @@ public class UserController {
         return this.userService.findSorted(keyword, sortBy, page, pageSize);
     }
 
-    @PostMapping(value = "/send-email")
-    public void sendEmail() {
-        emailVerificationService.sendEmail("wukenny0126@gmail.com", "Test Subject", "Hello from WatGuessr!");
-    }
-
-    @PostMapping("/send-otp")
-    public String sendOtp(@RequestParam String to) {
-        emailVerificationService.prepareToSendEmail(to);
-        return "OTP sent to " + to;
-    }
-
     @GetMapping(value = "/leaderboard")
     public QueryResults<LeaderboardUser> getLeaderboard(
             @RequestParam(required = false) String searchTerm,
@@ -76,15 +42,5 @@ public class UserController {
             @RequestParam(required = false, defaultValue = "10") Integer limit) {
 
         return this.userService.getLeaderboard(searchTerm, sortBy, limit, offset);
-    }
-
-    @PostMapping("/verify-otp")
-    public ResponseEntity<Void> verifyOtp(@RequestParam String email, @RequestParam String submittedOtp) {
-        var success = emailVerificationService.verify(email, submittedOtp); // checks + side effects. String email, String submittedCode
-        if (success) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.status(400).build();
-        }
     }
 }
