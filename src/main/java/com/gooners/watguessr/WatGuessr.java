5c4b0f36d5647e2cdb37d6f2520f9f7809351166
kennywu <wukenny0126@gmail.com>
2025-08-17 20:28:00 -0700

    75: Refactored credentials to ./env file

--- src/main/java/com/gooners/watguessr/WatGuessr.java
+++ src/main/java/com/gooners/watguessr/WatGuessr.java
@@ -1,7 +1,10 @@
 package com.gooners.watguessr;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -9,6 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class WatGuessr {
 	public static void main(String[] args) {
 		SpringApplication.run(WatGuessr.class, args);
+	}
+
+	@Autowired
+	private Environment env;
+
+	@PostConstruct
+	public void checkEnv() {
+		System.out.println("Mail host = " + env.getProperty("SPRING_MAIL_HOST"));
 	}
 }
 
