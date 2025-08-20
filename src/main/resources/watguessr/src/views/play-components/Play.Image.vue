ec08906e68cf7135ea0a57dd02a123de1f573064
reiwei <rachelqingranwei@gmail.com>
2025-08-19 21:02:59 -0700

    137: Added preloading image to round 1, fixed saving of map position

--- src/main/resources/watguessr/src/views/play-components/Play.Image.vue
+++ src/main/resources/watguessr/src/views/play-components/Play.Image.vue
@@ -2,17 +2,33 @@
 import { mapGetters } from 'vuex'
 
 export default {
+  emits: ['image-loaded', 'image-error'],
   computed: {
     ...mapGetters({
       imageUrl: 'round/getImageUrl'
     })
+  },
+  methods: {
+    handleImageLoad() {
+      this.$emit('image-loaded')
+    },
+    handleImageError() {
+      this.$emit('image-error')
+    }
   }
 }
 </script>
 
 <template>
   <div class="image-root">
-    <img v-if="imageUrl" :src="imageUrl" alt="round scene" id="image" />
+    <img 
+      v-if="imageUrl" 
+      :src="imageUrl" 
+      alt="round scene" 
+      id="image" 
+      @load="handleImageLoad" 
+      @error="handleImageError"
+    />
     <div v-else id='image'></div>
   </div>
 </template>
