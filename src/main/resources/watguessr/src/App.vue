b063bb3e43ff239097dad2963dabc595378e1fb2
reiwei <rachelqingranwei@gmail.com>
2025-08-19 22:49:58 -0700

    got rid of styling around the click here to log in section

--- src/main/resources/watguessr/src/App.vue
+++ src/main/resources/watguessr/src/App.vue
@@ -338,10 +338,7 @@ onMounted(() => {
 
 .report-bug-sidebar {
   margin-bottom: 16px;
-  border-radius: 12px;
-  padding: 16px;
-  background: rgba(255, 255, 255, 0.05);
-  border: 1px solid rgba(255, 255, 255, 0.1);
+  padding: 8px;
 }
 
 .report-bug-sidebar h4 {
