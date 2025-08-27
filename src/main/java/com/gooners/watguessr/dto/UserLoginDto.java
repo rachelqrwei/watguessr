420d4fc2a88a9df8a875b54bff7cccd8e065c3c3
kennywu <wukenny0126@gmail.com>
2025-08-27 11:39:36 -0700

    #205: Added remember me to login

--- src/main/java/com/gooners/watguessr/dto/UserLoginDto.java
+++ src/main/java/com/gooners/watguessr/dto/UserLoginDto.java
@@ -3,6 +3,7 @@ package com.gooners.watguessr.dto;
 public class UserLoginDto {
     private String username;
     private String password;
+    private boolean rememberMe;
 
     public String getPassword() {
         return password;
@@ -18,5 +19,13 @@ public class UserLoginDto {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
     }
 }
